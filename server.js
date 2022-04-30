ca72842e8fb1304489d7d7adfcd3d076130e8121
carolynhammond <caroham@live.unc.edu>
2022-04-30 17:58:51 -0400

    comment

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html')
     res.end(data)
 })
+
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
 server.listen(port, () => {
